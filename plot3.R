2367b6f4ff0f7ea8f685559eae522dd5a6636707
summermeng <mengxia0912@gmail.com>
2014-07-13 10:17:22 -0700

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,8 @@
+household_power_consumption <- read.csv("C:/Users/Mengxia/Downloads/exdata-data-household_power_consumption/household_power_consumption.txt", sep=";")
+hpc<-household_power_consumption
+attach(hpc)
+mydata <- hpc[66638:69517, ]
+dim(mydata)
+Global_active_power <- as.numeric(mydata$V3)
+time <- c(1:2880)
+plot(time, V5, type="l", ylab="Voltage", xlab="datetime")
